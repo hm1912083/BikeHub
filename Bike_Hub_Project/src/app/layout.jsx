5a1b7305cd2d985c3b5b472f18e4a0c84bf461fd
Hammad M. Imtiaz <hm1912083@qu.edu.qa>
2024-05-13 01:44:39 +0300

    Commented app.css

--- Bike_Hub_Project/src/app/layout.jsx
+++ Bike_Hub_Project/src/app/layout.jsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer' 
-import Styles from '../../public/css/App.css'
+// import Styles from'../../public/css/App.css'
+// import Styles from'../../public/css/App.css'
 
 export default function RootLayout({ children }) {
   return (
