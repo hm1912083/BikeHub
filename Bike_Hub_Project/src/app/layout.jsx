22ffeb0682c6b28b515ebceaae6f2f625b4b37a3
Hammad M. Imtiaz <hm1912083@qu.edu.qa>
2024-05-13 01:21:35 +0300

    Phase 2 - style from app.css

--- Bike_Hub_Project/src/app/layout.jsx
+++ Bike_Hub_Project/src/app/layout.jsx
@@ -1,6 +1,6 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer' 
-import '../../public/css/App.css'
+import Styles from '../../public/css/App.css'
 
 export default function RootLayout({ children }) {
   return (
